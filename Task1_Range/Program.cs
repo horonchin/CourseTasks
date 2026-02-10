5cdfd7e562f6506224fd578e574b1c200e561261
horonchin <horonchin@yandex.ru>
2026-02-10 19:58:00 +0700

    Добавьте файлы проекта.

---
+++ Task1_Range/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+	public static void Main()
+	{
+		Console.Write("Введите начало числового диапазона: ");
+		double rangeFrom = Convert.ToDouble(Console.ReadLine());
+
+		Console.Write("Введите конец числового диапазона: ");
+		double rangeTo = Convert.ToDouble(Console.ReadLine());
+
+		Range range = new Range(rangeFrom, rangeTo);
+
+		Console.WriteLine($"Длина отрезка равна {range.GetLength():F2}");
+
+		Console.Write("Введите число: ");
+		double inputNumber = Convert.ToDouble(Console.ReadLine());
+
+		if (range.IsInside(inputNumber))
+		{
+			Console.WriteLine($"Данное число пренадлежит диапазону от {range.From} до {range.To}");
+		}
+		else
+		{
+			Console.WriteLine($"Данное число не пренадлежит диапазону от {range.From} до {range.To}");
+		}
+	}
+}
---
+++ Task1_Range/Range.cs
@@ -0,0 +1,24 @@
+﻿class Range
+{
+	private const double Epsilon = 1.0e-10;
+
+	public double From { get; set; }
+
+	public double To { get; set; }
+
+	public Range(double from, double to)
+	{
+		From = from;
+		To = to;
+	}
+
+	public double GetLength()
+	{
+		return To - From;
+	}
+
+	public bool IsInside(double number)
+	{
+		return number >= From - Epsilon && number <= To + Epsilon;
+	}
+}
