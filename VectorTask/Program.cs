e510590f172caf52352a7e885952f75e28ef019d
horonchin <horonchin@yandex.ru>
2026-02-24 21:11:04 +0700

    fix vector

---
+++ VectorTask/Program.cs
@@ -0,0 +1,58 @@
+﻿using VectorTask;
+
+class Program
+{
+	public static void Main()
+	{
+		Vector vector1 = new Vector(5);
+		Console.WriteLine(vector1);
+
+		vector1[0] = 1.4;
+		vector1[1] = 2.5;
+		vector1[2] = 3.3;
+		vector1[3] = 5.5;
+		vector1[4] = 7;
+		Console.WriteLine(vector1);
+
+		Vector vector2 = new Vector(vector1);
+		Console.WriteLine(vector2);
+
+		double[] array1 = { 1.1, 2.2, 3.3, 4.4 };
+		Vector vector3 = new Vector(array1);
+		Console.WriteLine(vector3);
+		
+		Vector vector4 = new Vector(5, array1);
+		Console.WriteLine(vector4);
+
+		Console.WriteLine($"Длина вектора {vector1} равна: {vector1.Length}");
+
+		vector1.Add(vector2);
+		Console.WriteLine($"Сложение vector1 и vector2 равно: {vector1}");
+
+		vector1.Subtract(vector2);
+		Console.WriteLine($"Вычитание vector1 и vector2 равно {vector1}");
+
+		Vector vector5 = Vector.GetSum(vector2, vector1);
+		Console.WriteLine($"Сумма векторов {vector1} и {vector2} равно вектору vector5: {vector5}");
+
+		vector1.Negate();
+		Console.WriteLine($"Развёрнутый vector1 равен: {vector1}");
+
+		Vector vector6 = Vector.GetSum(vector2, vector3);
+		Console.WriteLine($"Cумма векторов {vector3} и {vector2} равна вектору vector6: {vector6}");
+
+		vector6.Multiply(5);
+		Console.WriteLine($"Умножение вектора vector6 на скаляр 5 равно: {vector6}");
+
+		Vector vector7 = Vector.GetSubtract(vector6,vector4);
+		Console.WriteLine($"Разность вектора {vector4} и {vector6} равна: {vector7}");
+
+		vector7[0] = 5;
+		double firstvector7Component = vector7[0];
+		Console.WriteLine($"Первый элемент вектора vector7 был изменён и стал равен: {firstvector7Component}");
+
+		Console.WriteLine($"Скалярное произведение вектора vector7 на vector4 равно: {Vector.GetDot(vector4, vector7)}");
+
+		Console.WriteLine($"Длина вектора {vector7} равна {vector7.GetSize()}");
+	}
+}
---
+++ VectorTask/Vector.cs
@@ -0,0 +1,219 @@
+﻿namespace VectorTask;
+
+class Vector
+{
+	private double[] _components;
+
+	public int Length => _components.Length;
+
+	public Vector(int vectorLength)
+	{
+		if (vectorLength <= 0)
+		{
+			throw new ArgumentException($"Длина вектора должна быть положительной: {vectorLength}", nameof(vectorLength));
+		}
+
+		_components = new double[vectorLength];
+	}
+
+	public Vector(Vector vector)
+	{
+		ArgumentNullException.ThrowIfNull(vector);
+
+		_components = new double[vector.Length];
+		Array.Copy(vector._components, _components, vector.Length);
+	}
+
+	public Vector(double[] components)
+	{
+		ArgumentNullException.ThrowIfNull(components);
+
+		_components = new double[components.Length];
+		Array.Copy(components, _components, components.Length);
+	}
+
+	public Vector(int vectorLength, double[] components)
+	{
+		ArgumentNullException.ThrowIfNull(components);
+
+		if (vectorLength <= 0)
+		{
+			throw new ArgumentException($"Длина вектора должна быть положительной: {vectorLength}", nameof(vectorLength));
+		}
+
+		_components = new double[vectorLength];
+
+		int copyLength = Math.Min(vectorLength, components.Length);
+		Array.Copy(components, 0, _components, 0, copyLength);
+	}
+
+	public override string ToString()
+	{
+		return $"{{{string.Join(", ", _components)}}}";
+	}
+
+	public double GetSize()
+	{
+		double sum = 0;
+
+		for (int i = 0; i < Length; i++)
+		{
+			sum += _components[i] * _components[i];
+		}
+
+		return Math.Sqrt(sum);
+	}
+
+	public void Add(Vector vector)
+	{
+		ArgumentNullException.ThrowIfNull(vector);
+
+		if (vector.Length > Length)
+		{
+			Array.Resize(ref _components, vector.Length);
+		}
+
+		for (int i = 0; i < vector.Length; i++)
+		{
+			_components[i] += vector._components[i];
+		}
+	}
+
+	public static Vector GetSum(Vector vector1, Vector vector2)
+	{
+		ArgumentNullException.ThrowIfNull(vector1);
+		ArgumentNullException.ThrowIfNull(vector2);
+
+		Vector result = new Vector(vector1);
+
+		result.Add(vector2);
+
+		return result;
+	}
+
+	public static Vector GetSubtract(Vector vector1, Vector vector2)
+	{
+		ArgumentNullException.ThrowIfNull(vector1);
+		ArgumentNullException.ThrowIfNull(vector2);
+
+		Vector result = new Vector(vector1);
+
+		result.Subtract(vector2);
+
+		return result;
+	}
+
+	public void Subtract(Vector vector)
+	{
+		ArgumentNullException.ThrowIfNull(vector);
+
+		if (vector.Length > Length)
+		{
+			Array.Resize(ref _components, vector.Length);
+		}
+
+		for (int i = 0; i < vector.Length; i++)
+		{
+			_components[i] -= vector._components[i];
+		}
+	}
+
+	public void Multiply(double n)
+	{
+		for (int i = 0; i < Length; i++)
+		{
+			_components[i] *= n;
+		}
+	}
+
+	public void Negate()
+	{
+		for (int i = 0; i < Length; i++)
+		{
+			_components[i] = -_components[i];
+		}
+	}
+
+	public static double GetDot(Vector vector1, Vector vector2)
+	{
+		ArgumentNullException.ThrowIfNull(vector1);
+		ArgumentNullException.ThrowIfNull(vector2);
+
+		double result = 0;
+		int minSize = Math.Min(vector1.Length, vector2.Length);
+
+		for (int i = 0; i < minSize; i++)
+		{
+			result += vector1._components[i] * vector2._components[i];
+		}
+
+		return result;
+	}
+
+	public double this[int index]
+	{
+		get
+		{
+			if (index < 0 || index >= _components.Length)
+			{
+				throw new IndexOutOfRangeException($"Индекс {index} вне границ (0-{_components.Length - 1})");
+			}
+
+			return _components[index];
+		}
+
+		set
+		{
+			if (index < 0 || index >= _components.Length)
+			{
+				throw new IndexOutOfRangeException($"Индекс {index} вне границ (0-{_components.Length - 1})");
+			}
+
+			_components[index] = value;
+		}
+	}
+
+	public override int GetHashCode()
+	{
+		const int prime = 17;
+
+		int hash = 1;
+
+		foreach (double component in _components)
+		{
+			hash = hash * prime + component.GetHashCode();
+		}
+
+		return hash;
+	}
+
+	public override bool Equals(object? obj)
+	{
+		if (ReferenceEquals(obj, this))
+		{
+			return true;
+		}
+
+		if (obj is null || GetType() != obj.GetType())
+		{
+			return false;
+		}
+
+		Vector otherVector = (Vector)obj;
+
+		if (otherVector.Length != Length)
+		{
+			return false;
+		}
+
+		for (int i = 0; i < Length; i++)
+		{
+			if (_components[i] != otherVector._components[i])
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
